0e31df60fc492b34ef9282d7c009a96756874e18
bessimaestro <bessimaestro@gmail.com>
2018-07-19 14:06:12 +0200

    more tests added

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const sum = require('../sum');
 
+/* common matchers */
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
@@ -18,6 +19,7 @@ test('adding positive numbers is not zero', () => {
     }
 });
 
+/* truthiness */
 test('wheter n is null', () => {
     const n = null;
     expect(n).toBeNull();
@@ -34,4 +36,59 @@ test('whether z is zero', () => {
     expect(z).not.toBeUndefined();
     expect(z).not.toBeTruthy();
     expect(z).toBeFalsy();
+});
+
+/* numbers */
+test('two plus two', () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
+
+    // toBe and toEqual are equivalent for numbers
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
+});
+
+test('adding floating point numbers', () => {
+    const value = 0.1 + 0.2;
+    //expect(value).toBe(0.3);           This won't work because of rounding error
+    expect(value).toBeCloseTo(0.3); // This works.
+});
+
+/* strings */
+test('there is no I in team', () => {
+    expect('team').not.toMatch(/I/);
+});
+
+test('but there is a "stop" in Christoph', () => {
+    expect('Christoph').toMatch(/stop/);
+});
+
+/* arrays */
+const shoppingList = [
+    'diapers',
+    'kleenex',
+    'trash bags',
+    'paper towels',
+    'beer',
+];
+
+test('the shopping list has beer on it', () => {
+    expect(shoppingList).toContain('beer');
+});
+
+/* exceptions */
+function compileAndroidCode() {
+    throw new Error('you are using the wrong JDK');
+}
+
+test('compiling android goes as expected', () => {
+    expect(compileAndroidCode).toThrow();
+    expect(compileAndroidCode).toThrow(Error);
+
+    // You can also use the exact error message or a regexp
+    expect(compileAndroidCode).toThrow('you are using the wrong JDK');
+    expect(compileAndroidCode).toThrow(/JDK/);
 });
