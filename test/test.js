3d8643bc2ff33917dfb5715561ca54ab415c837a
bessimaestro <bessimaestro@gmail.com>
2018-07-19 09:43:15 +0200

    refactoring

--- test/test.js
+++ test/test.js
@@ -25,13 +25,13 @@ test('wheter n is null', () => {
     expect(n).not.toBeUndefined();
     expect(n).not.toBeTruthy();
     expect(n).toBeFalsy();
-  });
-  
-  test('whether z is zero', () => {
+});
+
+test('whether z is zero', () => {
     const z = 0;
     expect(z).not.toBeNull();
     expect(z).toBeDefined();
     expect(z).not.toBeUndefined();
     expect(z).not.toBeTruthy();
     expect(z).toBeFalsy();
-  });
+});
